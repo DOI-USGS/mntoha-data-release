14b146da195b49cbb467e0a03f9c532dd8b02e9c
Hayley Corson-Dosch <hcorson-dosch@igskmncnpshtl02.tallgrass.cr.usgs.gov>
2020-12-01 13:18:10 -0600

    added launch script for jlab

---
+++ launch-jlab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JPORT=`shuf -i 8400-9400 -n 1`
+
+export PATH="$PATH:$HOME/miniconda3/bin"
+source activate lakes_pgdl
+
+echo "ssh -N -L $JPORT:`hostname`:$JPORT $USER@tallgrass.cr.usgs.gov"
+
+jupyter lab --ip '*' --no-browser --port $JPORT --notebook-dir=. &
+
+wait
